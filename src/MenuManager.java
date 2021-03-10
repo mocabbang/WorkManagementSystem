20041def6f3a07653195826a693dbd34c8546052
mocabbang <gjtod1621@naver.com>
2021-03-10 23:46:15 +0900

    -removed Hello.java, -added MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,10 +1,14 @@
 
-public class Hello 
+public class MenuManager 
 {
 
 	public static void main(String[] args) 
 	{
-		System.out.print("Hello");
+		System.out.print("1. Add Work");
+		System.out.print("2. Delete Work");
+		System.out.print("3. Edit Work");
+		System.out.print("4. Veiw Work");
+		System.out.print("5. Exit");
 	}
 
 }
