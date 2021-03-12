802d473462efffda42fd8c88294b9e1f61ffb717
mocabbang <gjtod1621@naver.com>
2021-03-12 20:13:48 +0900

    typing miss fixed

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -7,7 +7,7 @@ public class MenuManager
 		System.out.println("1. Add Work");
 		System.out.println("2. Delete Work");
 		System.out.println("3. Edit Work");
-		System.out.println("4. Veiw Work");
+		System.out.println("4. View Work");
 		System.out.println("5. Exit");
 	}
 }
