58e016c74e9e48e0a46dddd6fb9b2d522d82fb20
mocabbang <gjtod1621@naver.com>
2021-03-12 20:09:22 +0900

    print->println

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -4,11 +4,10 @@ public class MenuManager
 
 	public static void main(String[] args) 
 	{
-		System.out.print("1. Add Work");
-		System.out.print("2. Delete Work");
-		System.out.print("3. Edit Work");
-		System.out.print("4. Veiw Work");
-		System.out.print("5. Exit");
+		System.out.println("1. Add Work");
+		System.out.println("2. Delete Work");
+		System.out.println("3. Edit Work");
+		System.out.println("4. Veiw Work");
+		System.out.println("5. Exit");
 	}
-
 }
